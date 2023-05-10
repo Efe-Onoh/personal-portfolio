4e4f0094e1ff2f5f2629a39fdc9f190c260bb3bf
Efe O <ednaellenpope@gmail.com>
2023-05-11 01:18:27 +0400

    back to the hash

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Portfolio from "../components/portfolio/Portfolio";
 import Projects from "../components/project/Projects";
 
 export default function IndexRouter({projects}) {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Portfolio projects={projects}/>}>
           <Route index element={<h1>Select a Project</h1>}/>
@@ -13,6 +13,6 @@ export default function IndexRouter({projects}) {
           <Route path="*" element={<>Error: No Page Found</>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
