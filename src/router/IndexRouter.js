562b3c9964ec78ef17b9d949d8bfb53fb6f0f2d2
Efe O <ednaellenpope@gmail.com>
2023-05-11 01:04:33 +0400

    testing diffs

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Portfolio from "../components/portfolio/Portfolio";
 import Projects from "../components/project/Projects";
 
 export default function IndexRouter({projects}) {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Portfolio projects={projects}/>}>
           <Route path="projects/:projectId" element={<Projects projects={projects} />} /> 
           <Route path="*" element={<>Error: No Page Found</>}/>
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
