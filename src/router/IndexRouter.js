91919d4f27947ef36ffc93664d0526ed3498dcfe
Efe O <ednaellenpope@gmail.com>
2023-05-11 00:34:19 +0400

    mod to HashRouter

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import Portfolio from "../components/portfolio/Portfolio";
 import Projects from "../components/project/Projects";
 
 export default function IndexRouter({projects}) {
   return (
-    <BrowserRouter basename="https://efe-onoh.github.io/personal-portfolio">
+    <HashRouter basename="https://efe-onoh.github.io/personal-portfolio">
       <Routes>
         <Route path="/" element={<Portfolio projects={projects}/>}>
           <Route path="projects/:projectId" element={<Projects projects={projects} />} /> 
           <Route path="*" element={<>Error: No Page Found</>}/>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
