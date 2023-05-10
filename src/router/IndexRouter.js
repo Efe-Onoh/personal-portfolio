2ddb0c2c7c354f242852348fad18bc8640fc0513
Efe O <ednaellenpope@gmail.com>
2023-05-11 01:10:17 +0400

    index outlet

--- src/router/IndexRouter.js
+++ src/router/IndexRouter.js
@@ -8,6 +8,7 @@ export default function IndexRouter({projects}) {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Portfolio projects={projects}/>}>
+          <Route index element={<h1>Select a Project</h1>}/>
           <Route path="projects/:projectId" element={<Projects projects={projects} />} /> 
           <Route path="*" element={<>Error: No Page Found</>}/>
         </Route>
